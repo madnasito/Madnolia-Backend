29bd878f06aeb600d2782ac980ef0180d5765523
MADNA <madnamv@gmail.com>
2023-02-20 13:19:11 -0400

    Fix: base_url

--- public/main.js
+++ public/main.js
@@ -1247,7 +1247,7 @@ __webpack_require__.r(__webpack_exports__);
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 const environment = {
-    production: false,
+    production: true,
     base_url: 'https://sea-turtle-app-pq546.ondigitalocean.app/'
 };
 /*
