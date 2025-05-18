ee6c36f4060df42ad9518bdc05c1f2cbf517d55a
MADNA <madnamv@gmail.com>
2025-05-17 21:36:48 -0400

    render

--- public/server/angular-app-manifest.mjs
+++ public/server/angular-app-manifest.mjs
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 34972, hash: '3a3ccf62ce31713e5f05e6539e5e8fece647a58590f0049e8c7c2d4f1439fcf9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1521, hash: '89e3de448a02d89e1346a5c3e0a331b11919ec9319cb2c17c6128ceb9935eb7e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 34972, hash: '9dffcb0f81cfe05ff5d726dc7466182061d2fe7dea8708f90e25c161480995dd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1521, hash: '0028b9d6c02755859ce59998251f1fb56e576a7311a79819c7eb130e5d117cb1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-JTUCIHJV.css': {size: 301239, hash: 'R0RhmDrWMTc', text: () => import('./assets-chunks/styles-JTUCIHJV_css.mjs').then(m => m.default)}
   },
 };
