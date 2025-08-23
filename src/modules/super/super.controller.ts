fa6303a84fc7d38ca1328f35f93518e1b7db6f0f
MADNA <madnamv@gmail.com>
2025-08-23 08:54:46 -0400

    Add: Required password to delete account

--- src/modules/super/super.controller.ts
+++ src/modules/super/super.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Delete, UseGuards, Request } from '@nestjs/common';
+import { Controller, Delete, UseGuards, Request, Body } from '@nestjs/common';
 import { UserGuard } from 'src/common/guards/user.guard';
 import { SuperService } from './super.service';
+import { UserPasswordDto } from './dto/user-password.dto';
 
 @Controller('super')
 export class SuperController {
   constructor(private readonly superService: SuperService) {}
   @Delete('user')
   @UseGuards(UserGuard)
-  deleteUserFromDb(@Request() req: any) {
-    return this.superService.deleteUser(req.user.id);
+  deleteUserFromDb(@Request() req: any, @Body() body: UserPasswordDto) {
+    return this.superService.deleteUser(req.user.id, body.password);
   }
 }
