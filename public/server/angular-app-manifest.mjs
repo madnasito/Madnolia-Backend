673d02bb6bdae54313427e765b25f4d93ec389c0
MADNA <madnamv@gmail.com>
2025-08-23 09:40:16 -0400

    xd

--- public/server/angular-app-manifest.mjs
+++ public/server/angular-app-manifest.mjs
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 35060, hash: 'd34e05d6af1f0d6622aff832731380b40938cbee6e8086b94f4fb77c48bdeb4b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1609, hash: 'c4de9e4928d7bbc8df00c5f390ae626cb5e49ec233d4569386258d340854d186', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 35060, hash: 'f0c7fc43ae26e9fa0b7bdf87981999ce0479acd08f80ada621c9a2d653c36879', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1609, hash: 'e964401579f934b98724fc016b639af2dcd883ba93d4d7e07fd5aab6b0346e04', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-IIZIQCKJ.css': {size: 301304, hash: 'kcooMvmwg3Y', text: () => import('./assets-chunks/styles-IIZIQCKJ_css.mjs').then(m => m.default)}
   },
 };
