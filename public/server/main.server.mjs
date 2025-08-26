de6d64a179e52aae620f3e5e663559d550d78278
MADNA <madnamv@gmail.com>
2025-08-25 21:02:48 -0400

    Testing metatags

--- public/server/main.server.mjs
+++ public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{e,f as t,h as o,i as p,k as m}from"./chunk-2AGQ5Q6W.mjs";import{Mb as r}from"./chunk-5SD657DG.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
+import{e,f as t,h as o,i as p,k as m}from"./chunk-PENPZ36J.mjs";import{Mb as r}from"./chunk-5SD657DG.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
