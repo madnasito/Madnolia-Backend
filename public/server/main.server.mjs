48c4f96857d47f1f3a9bcf0dab0c86eedd7024f6
MADNA <madnamv@gmail.com>
2025-08-26 21:46:37 -0400

    XD

--- public/server/main.server.mjs
+++ public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{e,f as t,h as o,i as p,j as m}from"./chunk-I6TVSWIS.mjs";import{Mb as r}from"./chunk-5SD657DG.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
+import{e,f as t,h as o,i as p,j as m}from"./chunk-NVLG5VOV.mjs";import{Mb as r}from"./chunk-5SD657DG.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
