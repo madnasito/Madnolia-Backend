2f99a758826a6bb56775e0e8d421ebaca4240ffe
MADNA <madnamv@gmail.com>
2025-09-28 05:46:38 -0400

    Update: send image too

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -245,7 +245,7 @@ export class UsersService {
         $nor: [{ _id: userId }],
         status: true,
       })
-      .select({ name: 1, username: 1, _id: 1, thumb: 1 })
+      .select({ name: 1, username: 1, _id: 1, thumb: 1, image: 1 })
       .lean()
       .skip(skip)
       .limit(limit)
